5a6429fcc54c64b316dba58733785b271fca891a
Recep Karamanlı <64321921+recepkaramanli@users.noreply.github.com>
2020-04-25 20:22:04 +0300

    Add files via upload

--- sunucu.js
+++ sunucu.js
@@ -7,7 +7,7 @@ app.get('/', async function (req, res) {
   let buff = new Buffer(req.params.data, 'base64')
   let text = buff.toString('ascii')
 
-  let driver = await new webdriver.Builder().forBrowser('chrome').build();
+  let driver = await new webdriver.Builder().forBrowser('firefox').build();
   await driver.get('https://www.recepkaramanli.com')
   await driver.getPageSource()
     .then(function (src) {
