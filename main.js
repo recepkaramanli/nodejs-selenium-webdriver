898bd847257f4ca1d2e56d214326fc7e6e09f1f8
Recep Karamanlı <64321921+recepkaramanli@users.noreply.github.com>
2020-04-26 12:20:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-const webdriver = require('selenium-webdriver');
+const webdriver = require('selenium-webdriver')
+const path = require('path')
+
+// Serve static files from the React app. 
+app.use(express.static(path.join(__dirname, '..', 'client/build')));
+
+
 
 async function sayfaac(url) {
+  
+  require('chromedriver');
+  let options = new chrome.Options();
+  let serviceBuilder = new chrome.ServiceBuilder(process.env.CHROME_DRIVER_PATH);
 
-  let driver =  await new webdriver.Builder().forBrowser('chrome').build();
+    //Don't forget to add these for heroku
+    options.addArguments("--headless");
+    options.addArguments("--disable-gpu");
+    options.addArguments("--no-sandbox");
+  
+  
+  let driver =  await new webdriver.Builder().
+  forBrowser('chrome').
+  setChromeOptions(options).
+  setChromeService(serviceBuilder).
+  build();
   await driver.get('https://www.sahibinden.com/')
   driver.getPageSource()
   .then(function(src) {
