9f0190cc13cfa0924042538f358e99fece8eb2a0
Recep Karamanlı <64321921+recepkaramanli@users.noreply.github.com>
2020-05-03 22:28:05 +0300

    Update main.js

--- main.js
+++ main.js
@@ -10,8 +10,6 @@ options.addArguments('--user-data-dir=chrome-data');
 
 async function sayfaac() {
     let driver = await new webdriver.Builder().forBrowser('chrome').setChromeOptions(options).build();
-    //await driver.get('https://www.sahibinden.com/ilan/ikinci-el-ve-sifir-alisveris-bilgisayar-dizustu-no$
-    //await driver.get('https://www.recepkaramanli.com/fiyat-takip/islemtest.php');
     await driver.get('https://www.recepkaramanli.com/fiyat-takip');
     await driver.getTitle().then(function(title) { console.log(title); })
     //await driver.getPageSource().then(function (src) { console.log(src); });
